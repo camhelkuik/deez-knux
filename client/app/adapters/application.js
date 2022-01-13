cfce053e7fc0d9dba5871d81d07bd47a55745296
Cam Helkuik <cameron@travefy.com>
2022-01-12 19:54:03 -0600

    updated adapter

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -2,20 +2,23 @@ import JSONAPIAdapter from '@ember-data/adapter/json-api';
 // eslint-disable-next-line ember/no-mixins
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from '../config/environment';
+import { inject as service } from 'ember/service';
 
 export default class ApplicationAdapter extends JSONAPIAdapter.extend(
-  DataAdapterMixin,
+    DataAdapterMixin,
 ) {
-  host = ENV.apiHost;
+    @service session;
 
-  get headers() {
-    let headers = {};
+    host = ENV.apiHost;
 
-    if (this.session.isAuthenticated) {
-      const authHeader = `Bearer ${this.session.data.authenticated.access_token}`;
-      headers['Authorization'] = authHeader;
+    get headers() {
+        let headers = {};
+
+        if (this.session.isAuthenticated) {
+            const authHeader = `Bearer ${this.session.data.authenticated.access_token}`;
+            headers['Authorization'] = authHeader;
+        }
+
+        return headers;
     }
-
-    return headers;
-  }
 }
