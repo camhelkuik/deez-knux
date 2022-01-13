efb639d7ab593dfaea8ce3949fd830b82f638bb9
Cam Helkuik <cameron@travefy.com>
2022-01-13 09:28:17 -0600

    Have knux saving to the db again

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,24 +1,44 @@
+// import JSONAPIAdapter from '@ember-data/adapter/json-api';
+// // eslint-disable-next-line ember/no-mixins
+// import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+// import ENV from '../config/environment';
+// import { inject as service } from 'ember/service';
+
+// export default class ApplicationAdapter extends JSONAPIAdapter.extend(
+//     DataAdapterMixin,
+// ) {
+//     @service session;
+
+//     host = ENV.apiHost;
+
+//     get headers() {
+//         let headers = {};
+
+//         if (this.session.isAuthenticated) {
+//             const authHeader = `Bearer ${this.session.data.authenticated.access_token}`;
+//             headers['Authorization'] = authHeader;
+//         }
+
+//         return headers;
+//     }
+// }
+
+// app/adapters/application.js
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
-// eslint-disable-next-line ember/no-mixins
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
-import ENV from '../config/environment';
-import { inject as service } from 'ember/service';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default class ApplicationAdapter extends JSONAPIAdapter.extend(
-    DataAdapterMixin,
-) {
-    @service session;
+export default class ApplicationAdapter extends JSONAPIAdapter {
+  @service session;
 
-    host = ENV.apiHost;
-
-    get headers() {
-        let headers = {};
-
-        if (this.session.isAuthenticated) {
-            const authHeader = `Bearer ${this.session.data.authenticated.access_token}`;
-            headers['Authorization'] = authHeader;
-        }
-
-        return headers;
+  @computed('session.{data.authenticated.access_token,isAuthenticated}')
+  get headers() {
+    let headers = {};
+    if (this.session.isAuthenticated) {
+      // OAuth 2
+      headers['Authorization'] = `Bearer ${this.session.data.authenticated.access_token}`;
     }
+
+    return headers;
+  }
 }
