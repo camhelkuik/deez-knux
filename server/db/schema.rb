b302097bb1292069c6f7e7b10b8a0d2ac5bd93f5
Cam Helkuik <cameron@travefy.com>
2022-01-13 16:54:32 -0600

    adding door keeper

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_222512) do
+ActiveRecord::Schema.define(version: 2022_01_13_224611) do
 
   create_table "knuxes", force: :cascade do |t|
     t.string "knux"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2022_01_13_222512) do
     t.datetime "remember_created_at", precision: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
